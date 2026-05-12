abcc80f12d803b4029297552209c6fc13f02fd39
harmdeveloper <harmfatima046@gmail.com>
2026-05-13 00:30:32 +0500

    Update foodRoute.js

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -5,12 +5,7 @@ import multer  from 'multer'
 const foodRouter  = express.Router();
 
 //image storage engine
-const storage = multer.diskStorage({
-    destination:"uploads",
-    filename:(req,file,cb) => {
-       return cb(null,`${Date.now()}${file.originalname}`) 
-    }
-})
+const storage = multer.memoryStorage();
 //middleware
 const upload =  multer({storage:storage})
 
