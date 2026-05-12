1c8679d06bad60752336c341ac15497837696003
harmdeveloper <harmfatima046@gmail.com>
2026-05-12 23:20:40 +0500

    Change API URL from localhost to production

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -13,7 +13,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-  const url  = "http://localhost:4000"
+  const url  = "https://food-factory-backend.vercel.app"
 
   return (
     <div>
