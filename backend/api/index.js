99280f39e3a3be4e2fea2351f8fc81418598bc21
harmdeveloper <harmfatima046@gmail.com>
2026-05-13 00:22:36 +0500

    Import app from backend/server.js

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-
+import app from "../backend/server.js";
 import { connectDB } from "../config/db.js";
 import foodRouter from "../routes/foodRoute.js";
 import userRouter from "../routes/userRoute.js";
