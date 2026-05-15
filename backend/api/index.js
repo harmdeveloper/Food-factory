17fae4572374b55d4cdb3003539fa74de1a672f6
harmdeveloper <harmfatima046@gmail.com>
2026-05-15 17:47:46 +0500

    Update index.js

--- backend/api/index.js
+++ backend/api/index.js
@@ -26,7 +26,7 @@ app.use("/api/food", foodRouter);
 app.use("/api/user", userRouter);
 app.use("/api/cart", cartRouter);
 app.use("/api/order", orderRouter);
-app.use("/api", contactRouter);
+app.use("/api/contacts", contactRouter);
 
 // test route
 app.get("/", (req, res) => {
