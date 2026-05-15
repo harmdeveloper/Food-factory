6e58fccddd28f610228962f64fef94e81c7dfbed
harmdeveloper <harmfatima046@gmail.com>
2026-05-15 14:21:55 +0500

    Set up Express server with multiple API routes

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,1 +1,43 @@
 
+//basic module type server
+import express from "express"
+import cors from "cors"
+import { connectDB } from "./config/db.js"
+import foodRouter from "./routes/foodRoute.js"
+import userRouter from "./routes/userRoute.js"
+import 'dotenv/config'
+import cartRouter from "./routes/cartRoute.js"
+import orderRouter from "./routes/orderRoute.js"
+import contactRouter from "./routes/contactRoute.js"
+import aiRoute from "./routes/aiRoute.js";
+
+//app config
+const app = express();
+const port = process.env.PORT || 4000;
+
+//middleware
+app.use(express.json())
+
+//cors
+app.use(cors())
+
+//db connection
+connectDB();
+
+//api endpoints
+app.use("/api/food", foodRouter)
+app.use("/images", express.static('uploads'))
+app.use("/api/user", userRouter)
+app.use("/api/cart", cartRouter)
+app.use("/api/order", orderRouter)
+app.use("/api", contactRouter)
+app.use("/api/ai", aiRoute);
+
+//test route
+app.get("/", (req, res) => {
+    res.send("API Working")
+})
+
+app.listen(port, () => {
+    console.log(`Server Started on Port ${port}`)
+})
