a256e91a0d44e3d0dc98bddb75b0736f2b3c1eb6
harmdeveloper <harmfatima046@gmail.com>
2026-05-15 14:29:47 +0500

    Fix import path for connectDB in index.js

--- backend/api/index.js
+++ backend/api/index.js
@@ -2,7 +2,7 @@
 //basic module type server
 import express from "express"
 import cors from "cors"
-import { connectDB } from "./config/db.js"
+import connectDB from "../config/db.js"
 import foodRouter from "./routes/foodRoute.js"
 import userRouter from "./routes/userRoute.js"
 import 'dotenv/config'
