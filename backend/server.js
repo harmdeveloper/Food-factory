94d51d0875d72b9a394c9212a4b6c3ad246a10c1
harmdeveloper <harmfatima046@gmail.com>
2026-05-15 13:36:47 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,60 +1,43 @@
-import express from "express";
-import cors from "cors";
-import { connectDB } from "./config/db.js";
-import foodRouter from "./routes/foodRoute.js";
-import userRouter from "./routes/userRoute.js";
-import cartRouter from "./routes/cartRoute.js";
-import orderRouter from "./routes/orderRoute.js";
-import contactRouter from "./routes/contactRoute.js";
-import 'dotenv/config';
-import path from "path";
+//basic module type server
+import express from  "express"
+import cors from  "cors"
+import { connectDB } from "./config/db.js"
+import foodRouter from "./routes/foodRoute.js"
+import userRouter from "./routes/userRoute.js"
+import 'dotenv/config'
+import cartRouter from "./routes/cartRoute.js"
+import orderRouter from "./routes/orderRoute.js"
+import contactRouter from "./routes/contactRoute.js"
+import aiRoute from "./routes/aiRoute.js";
 
-// app config
-const app = express();
+//app config
+const app = express()
+// const port = 4000;
+const port = process.env.PORT || 4000;
 
-// middleware
-app.use(express.json());
+//middlware
+app.use(express.json())
+//access baclend from any front end
+app.use(cors())
 
-// CORS
-app.use(cors({
-  origin:[
-    "https://food-factory-frontend.vercel.app",
-    "https://food-factory-admin-swart.vercel.app"
-  ],
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
-}));
+//db connection
+connectDB();
 
-// db
-let isConnected = false;
-
-export const connectDB = async () => {
-  if (isConnected) return;
-
-  try {
-    await mongoose.connect(process.env.MONGO_URL);
-    isConnected = true;
-    console.log("DB Connected");
-  } catch (err) {
-    console.log("DB Error:", err.message);
-  }
-};
-
-startDB();
+//api endpoints
+app.use("/api/food",foodRouter)
+app.use("/images",express.static('uploads'))
+app.use("/api/user",userRouter)
+app.use("/api/cart",cartRouter)
+app.use("/api/order",orderRouter)
+app.use("/api",contactRouter)
+app.use("/api/ai", aiRoute);
 
 
-// routes
-app.use("/api/food", foodRouter);
-app.use("/images", express.static(path.join(process.cwd(), "uploads")));
-app.use("/api/user", userRouter);
-app.use("/api/cart", cartRouter);
-app.use("/api/order", orderRouter);
-app.use("/api", contactRouter);
+//http method to req data from  server
+app.get("/",(req,res)=>{
+    res.send("API Working")
+})
 
-// test route
-app.get("/", (req, res) => {
-  res.json({ message: "Backend running on Vercel 🚀" });
-});
-
-// ❌ NO app.listen in Vercel
-export default app;
+app.listen(port,()=>{
+    console.log(`Server Started on http://localhost:${port}`)
+})
