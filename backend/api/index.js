f2cf7a1a2bda05bccecf33ec2c320e2af53f05d8
harmdeveloper <harmfatima046@gmail.com>
2026-05-15 17:34:39 +0500

    Change contactRouter API route path

--- backend/api/index.js
+++ backend/api/index.js
@@ -26,7 +26,7 @@ app.use("/api/food", foodRouter);
 app.use("/api/user", userRouter);
 app.use("/api/cart", cartRouter);
 app.use("/api/order", orderRouter);
-app.use("/api/contacts", contactRouter);
+app.use("/api", contactRouter);
 
 // test route
 app.get("/", (req, res) => {
